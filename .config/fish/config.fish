88b7651ce6b321d86b0e5729ebd528aa3e4170c4
Nightmare Pog <44711407+NightmarePog@users.noreply.github.com>
2025-09-20 22:15:58 +0200

    feat: add wofi

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,8 @@ set -U fish_greeting ""
 set -x LANG en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
 starship init fish | source
+set -Ux ANDROID_HOME $HOME/Android/Sdk
+set -Ux PATH $PATH $ANDROID_HOME/emulator $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/platform-tools
 
     # Commands to run in interactive sessions can go here
 end
